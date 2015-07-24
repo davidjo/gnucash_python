f828e450ccfa46bf93633dfcd404f62735b1058c
djosg <djosg@invalid.local>
2015-07-24 21:51:48 +0100

    New implementation of python based tools using the new wrap of GncPlugin - all tool scripts in directory tools added.

---
+++ tool_objects.py
@@ -0,0 +1,74 @@
+
+
+# 
+
+import pdb
+
+
+class ToolTemplate(object):
+
+    # object new Tools should be subclassed from
+
+    def __init__ (self):
+
+        self.tool_guid = None
+        self.name = None
+        self.menu_name = None
+        self.menu_tip = None
+        self.menu_path = None
+        self.stock_id = None
+
+    def run (self):
+        raise NotImplementedError("The run method MUST be defined in the subclass!!")
+
+
+
+# we need these dicts to lookup the tool object on callback
+
+python_tools_by_name = {}
+python_tools_by_guid = {}
+
+def load_python_tools ():
+    # yes we need the global to write into these objects
+    global python_tools_by_name
+    global python_tools_by_guid
+
+    #pdb.set_trace()
+
+    python_tools_by_name = {}
+    python_tools_by_gui = {}
+
+    if False:
+
+        try:
+            from tools.hello_world import HelloWorld
+            python_tools_by_name['HelloWorld'] = HelloWorld()
+        except Exception, errexc:
+            traceback.print_exc()
+            pdb.set_trace()
+
+    if True:
+
+        # code to try for autoimporting - so can just add new tools
+        # only works assuming always use class name as tools name in python_tools
+
+        try:
+            import tools
+        except Exception, errexc:
+            traceback.print_exc()
+            pdb.set_trace()
+
+        # if we want to find all new classes another way is to use introspection
+        # and find all classes whose __module__ attribute is the module name it was imported
+        # under
+
+        # this is a very sneaky way to find new classes if all classes you want
+        # are guaranteed to be subclasses of a specific class
+        __all__classes = [ cls for cls in ToolTemplate.__subclasses__() ]
+
+        # instantiate all tool objects and save in the lookup dicts
+
+        for tool_cls in __all__classes:
+            python_tools_by_name[tool_cls.__name__] = tool_cls()
+            python_tools_by_guid[python_tools_by_name[tool_cls.__name__].tool_guid] = python_tools_by_name[tool_cls.__name__]
+
---
+++ tools/hello_world.py
@@ -0,0 +1,53 @@
+
+
+import gtk
+
+import pdb
+
+# how to do internationalization in python
+#import gettext
+#gettext.bindtextdomain('myapplication', '/path/to/my/language/directory')
+#gettext.textdomain('myapplication')
+#_ = gettext.gettext
+# dummy function for internationalization
+def N_(msg):
+    return msg
+
+import datetime
+
+from tool_objects import ToolTemplate
+
+class HelloWorld(ToolTemplate):
+
+    def __init__ (self):
+
+        super(HelloWorld,self).__init__()
+
+        # need to set the  base variables
+        self.version = 1
+        self.name = N_("Hello, World")
+        self.tool_guid = "1244f648c6c14dcbb5bf5c95061a3a83"
+        self.menu_name = N_("Sample Tool Action")
+        self.menu_tip = N_("A sample tool.")
+        #self.menu_path = N_("Tool Path")
+        #self.stock_id = None
+
+    def run (self):
+
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_position(gtk.WIN_POS_CENTER)
+        self.window.set_default_size(800,600)
+        self.window.set_border_width(0)
+        self.window.connect('destroy-event', self.destroy_cb)
+        self.window.connect('delete-event', self.delete_cb)
+        button = gtk.Button(label="My Button")
+        self.widget.add(button)
+        self.window.show_all()
+
+    def destroy_cb (self, actionobj, userdata=None):
+        self.window.destroy()
+
+    def delete_cb (self, actionobj, userdata=None):
+        self.window.destroy()
+
+
